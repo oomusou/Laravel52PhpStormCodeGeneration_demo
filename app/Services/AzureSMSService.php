9db85e766750a619ba89ce75987b3d3d718f0503
Sam.IO.Hsiao <sam.io.hsiao@benq.com>
2016-07-28 14:47:32 +0800

    自動實現 interface 的 method

---
+++ app/Services/AzureSMSService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+class AzureSMSService implements SMSInterface
+{
+    /**
+     * @param string $msg
+     * @return mixed
+     */
+    public function sendMessage(string $msg)
+    {
+        // TODO: Implement sendMessage() method.
+    }
+
+
+}
