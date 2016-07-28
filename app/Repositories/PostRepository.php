e9e14ebc547c7a805d78b4c880c6112172aaa31e
Sam.IO.Hsiao <sam.io.hsiao@benq.com>
2016-07-28 12:08:55 +0800

    PostRepository 自動 use namespace

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -5,19 +5,12 @@ declare(strict_types = 1);
 namespace App\Repositories;
 
 use App\Post;
+use Illuminate\Database\Eloquent\Collection;
 
 class PostRepository
 {
-    /**
-     * @param int $id
-     * @param string $default
-     * @return string
-     */
-    public function getTitle(int $id, string $default) : string
+    public function getAllPosts() : Collection
     {
-        return Post::whereId($id)
-            ->get()
-            ->first(null, new Post(['title' => $default]))
-            ->title;
+        return Post::all();
     }
 }
