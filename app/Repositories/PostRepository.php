79745623c4cbc82c7759e30d4cb8bdcda4cc8890
Sam.IO.Hsiao <sam.io.hsiao@benq.com>
2016-07-28 12:17:39 +0800

    PostRepository 自動刪除沒用的 namespace

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -4,13 +4,11 @@ declare(strict_types = 1);
 
 namespace App\Repositories;
 
-use App\Post;
 use Illuminate\Database\Eloquent\Collection;
 
 class PostRepository
 {
     public function getAllPosts() : Collection
     {
-        return Post::all();
     }
 }
